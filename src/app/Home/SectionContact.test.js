9955f3ae63593b89f69e25bf6bd0aff2402dd44a
Brian Park <brianpark25@gmail.com>
2018-03-27 23:13:22 -0700

    Add test for section contact

---
+++ src/app/Home/SectionContact.test.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Enzyme, { shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import SectionContact from './SectionContact'
+
+Enzyme.configure({ adapter: new Adapter() })
+
+describe('SectionContact', () => {
+  const sectionContact = shallow(<SectionContact />)
+
+  it('renders correctly', () => {
+    expect(sectionContact).toMatchSnapshot()
+  })
+
+  it('renders a styled social media bar', () => {
+    expect(sectionContact.find('Styled(BarSocialMedia)').exists()).toBe(true)
+  })
+})
